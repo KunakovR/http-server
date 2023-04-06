50bc5505de03519a11fb4f3a4a724e2f6b7c36e3
Ruslan <_>
2023-04-06 15:44:42 +0700

    Fixed second var

--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -17,17 +17,12 @@ public class Server {
     private static final List<String> VALID_PATHS = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     private final ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
-    public static void main(String[] args) {
-        new Server().start();
-    }
-
     public void start() {
         try (final var serverSocket = new ServerSocket(PORT)) {
             while (true) {
                 try {
                     final var socket = serverSocket.accept();
-                    Connection connection = new Connection(socket);
-                    threadPool.submit(connection);
+                    threadPool.submit(() -> handler(socket));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -37,73 +32,64 @@ public class Server {
         }
     }
 
-    class Connection implements Runnable {
+    private void handler(Socket socket) {
+        try {
+            final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            final var out = new BufferedOutputStream(socket.getOutputStream());
+            // read only request line for simplicity
+            // must be in form GET /path HTTP/1.1
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
 
-        private final Socket socket;
+            if (parts.length != 3) {
+                in.close();
+                out.close();
+                socket.close();
+            }
 
-        public Connection(Socket socket) {
-            this.socket = socket;
-        }
-
-        public void run() {
-            try {
-                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                final var out = new BufferedOutputStream(socket.getOutputStream());
-                // read only request line for simplicity
-                // must be in form GET /path HTTP/1.1
-                final var requestLine = in.readLine();
-                final var parts = requestLine.split(" ");
-
-                if (parts.length != 3) {
-                    in.close();
-                    out.close();
-                    socket.close();
-                }
-
-                final var path = parts[1];
-                if (!VALID_PATHS.contains(path)) {
-                    out.write((
-                            "HTTP/1.1 404 Not Found\r\n" +
-                                    "Content-Length: 0\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    out.flush();
-                }
-
-                final var filePath = Path.of(".", "public", path);
-                final var mimeType = Files.probeContentType(filePath);
-
-                // special case for classic
-                if (path.equals("/classic.html")) {
-                    final var template = Files.readString(filePath);
-                    final var content = template.replace(
-                            "{time}",
-                            LocalDateTime.now().toString()
-                    ).getBytes();
-                    out.write((
-                            "HTTP/1.1 200 OK\r\n" +
-                                    "Content-Type: " + mimeType + "\r\n" +
-                                    "Content-Length: " + content.length + "\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    out.write(content);
-                    out.flush();
-                }
-
-                final var length = Files.size(filePath);
+            final var path = parts[1];
+            if (!VALID_PATHS.contains(path)) {
                 out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + length + "\r\n" +
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
                                 "Connection: close\r\n" +
                                 "\r\n"
                 ).getBytes());
-                Files.copy(filePath, out);
                 out.flush();
-            } catch (IOException e) {
             }
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+
+            // special case for classic
+            if (path.equals("/classic.html")) {
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+            }
+
+            final var length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+        } catch (IOException e) {
         }
     }
 }
